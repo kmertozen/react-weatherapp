c7e13f230fde023d2ef52b36560bd1844b5ac85e
Mert <raltahook07@gmail.com>
2022-08-30 20:42:53 +0300

    tailwind implement edildi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import TurkeyMap from 'turkey-map-react';
+import { useState } from 'react';
 function App() {
+  const axios = require("axios");
+  const [temp, setTemp] = useState()
+  const [city, setCity] = useState()
+  const [isLoading, setIsLoading] = useState(false)
+  const handleClick = (name) => {
+    const city = name.name
+    const options = {
+      method: 'GET',
+      url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=c26f26cf3b8cddeacb893c7e07d1b7ba`,
+    };
+
+    axios.request(options)
+    .then(response => {
+      console.log(response.data);
+      setTemp(Math.trunc(response.data.main.temp))
+      setCity(name.name)
+      setIsLoading(true)
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <TurkeyMap onClick={(name) => handleClick(name)} />
+      {isLoading && 
+      (<h1 className="text-3xl font-bold"> sehir={city}
+        <br />
+        sicaklik={temp}
+      </h1>)}
     </div>
   );
 }
